26e61beb5b6e624bcceee1875674a46bc7277e8e
cmd <echo>
2024-01-23 23:40:36 +0700

    ini file Tugas Harian

---
+++ Tugasharian/src/Sepeda.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author ACER
+ */
+public class Sepeda{
+                int gir;
+    // method (mutator) dengan parameter
+    void setGir(int pertambahanGir) {
+         gir= gir+ pertambahanGir;
+    }
+    // method (accessor)
+    int getGir() {
+        return gir;
+    }
+}
